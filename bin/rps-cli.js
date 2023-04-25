2dd4494d75c5e16155c9983c2fcfd7a60d73962e
Aayush Singh <aayushkt@gmail.com>
2023-04-25 18:19:26 -0400

    print rules on error

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -4,13 +4,17 @@ import { rps } from '../lib/rpsls.js'
 
 const cliArgs = minimist(process.argv.slice(2))
 
+function printRules() {
+    console.log("Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper\n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors")
+}
+
 if (!(cliArgs.h == undefined && cliArgs.help == undefined)) {
     console.log("Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n\t-h, --help      display this help message and exit\n\t-r, --rules     display the rules and exit\n\nExamples:\n\tnode-rps        Return JSON with single player RPS result.\n\t\t\te.g. {\"player\":\"rock\"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\te.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}")
     process.exit(0)
 }
 
 if (!(cliArgs.r == undefined && cliArgs.rules == undefined)) {
-    console.log("Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper\n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors")
+    printRules()
     process.exit(0)
 }
 
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -4,13 +4,17 @@ import { rps } from '../lib/rpsls.js'
 
 const cliArgs = minimist(process.argv.slice(2))
 
+function printRules() {
+    console.log("Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n  - Scissors CUTS Paper\n- Paper COVERS Rock\n- Rock SMOOSHES Lizard\n- Lizard POISONS Spock\n- Spock SMASHES Scissors\n- Scissors DECAPITATES Lizard\n- Lizard EATS Paper\n- Paper DISPROVES Spock\n- Spock VAPORIZES Rock\n- Rock CRUSHES Scissors\n")
+}
+
 if (!(cliArgs.h == undefined && cliArgs.help == undefined)) {
     console.log("Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n-h, --help        display this help message and exit\n-r, --rules       display the rules and exit\n\nExamples:\nnode-rpsls        Return JSON with single player RPSLS result.\n\t\te.g. {\"player\":\"rock\"}\nnode-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.\n\t\te.g {\"player\":\"rock\",\"opponent\":\"Spock\",\"result\":\"lose\"}\n")
     process.exit(0)
 }
 
 if (!(cliArgs.r == undefined && cliArgs.rules == undefined)) {
-    console.log("Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n  - Scissors CUTS Paper\n- Paper COVERS Rock\n- Rock SMOOSHES Lizard\n- Lizard POISONS Spock\n- Spock SMASHES Scissors\n- Scissors DECAPITATES Lizard\n- Lizard EATS Paper\n- Paper DISPROVES Spock\n- Spock VAPORIZES Rock\n- Rock CRUSHES Scissors\n")
+    printRules()
     process.exit(0)
 }
 
